c77c95b53c5bec2db93d4791d16faaf0b28022e7
pawel <pawel.2020.navtop@gmail.com>
2022-06-19 15:26:22 +0300

    19JUNE_1

--- js/main.js
+++ js/main.js
@@ -820,7 +820,19 @@ function sliderUp() {
 }
 
 
-var swiper = new Swiper(".mySwiper", {});
+var swiper = new Swiper(".mySwiper", {
+    effect: "cube",
+    grabCursor: true,
+    cubeEffect: {
+        shadow: true,
+        slideShadows: true,
+        shadowOffset: 20,
+        shadowScale: 0.94,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+    },
+});
 
 
 // Просмотр фото галереии
