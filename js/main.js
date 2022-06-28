7328e2280c5bad88ebb50afa0ebaafc489ff3688
pawel <pawel.2020.navtop@gmail.com>
2022-06-28 17:15:40 +0300

    28JUNE_5

--- js/main.js
+++ js/main.js
@@ -7,7 +7,7 @@ window.onload = function () {
         document.body.classList.remove('loaded_hiding');
         boxDucks.classList.add('active');
         boxTitle.classList.add('active');
-    }, 11500);
+    }, 500);
 
     window.scrollTo(0, 0);
 
